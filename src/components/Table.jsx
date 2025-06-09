d6d246e0954621d5583928f1e130491d135c11a7
vrajgautam1 <vrajgautam123@gmail.com>
2025-06-09 10:26:28 +0530

    added changes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import "../assets/script";
+import React, { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
